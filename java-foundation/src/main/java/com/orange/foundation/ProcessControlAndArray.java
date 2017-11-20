1a872383a9720e514fd654fad690f6e3606591fd
orange-hch <756236155@qq.com>
2017-11-21 00:22:52 +0800

    no message

--- java-foundation/src/main/java/com/orange/foundation/ProcessControlAndArray.java
+++ java-foundation/src/main/java/com/orange/foundation/ProcessControlAndArray.java
@@ -1,25 +1,22 @@
 package com.orange.foundation;
 
+import java.util.Arrays;
+
 public class ProcessControlAndArray {
 	public static void main(String[] args){
 		//switchDemo();
 		arrayDemo();
 	}
 	
+	/**
+	 * 数组demo
+	 */
 	public static void arrayDemo(){
 //		初始化
 		int[] intArray1 = new int[]{1,3,4,5,5,5}; //静态初始化
 		int[] intArray2 = new int[4]; //动态初始化
-		String s1 = "";
-		for (int i : intArray1) {
-			s1 += i+",";
-		}
-		System.out.println("intArray1 = "+s1);
-		String s2 = "";
-		for (int i : intArray2) {
-			s2 += i+",";
-		}
-		System.out.println("intArray2 = "+s2);
+		System.out.println("intArray1 = "+Arrays.toString(intArray1));
+		System.out.println("intArray2 = "+Arrays.toString(intArray2));
 		
 //		使用foreach循环迭代数组元素时，并不能改变数组元素的值
 		String[] stringArray=new String[]{"Hello","World","!"};
@@ -27,7 +24,24 @@ public class ProcessControlAndArray {
 			string="gogogo";
 		}
 		System.out.println(stringArray[0]);
+		
+		int[] intArray3=new int[5];
+		Arrays.fill(intArray3, 5); //给所有元素赋值
+		System.out.println("intArray3 = "+Arrays.toString(intArray3));
+		
+		int[] intArray4 = new int[]{3,2,8,1,29,12,9};
+		Arrays.sort(intArray4); //排序
+		System.out.println("intArray4 = "+Arrays.toString(intArray4));
+		
+		System.out.println("8在数组的第" + Arrays.binarySearch(intArray4, 8)+"位"); //查找，数组必须是排好序的。返回的次序下标从0开始
+		
+		int[] intArray5 = Arrays.copyOf(intArray4, 4); //复制数组
+		System.out.println("intArray5 = "+Arrays.toString(intArray5));
+		
 	}
+	/**
+	 * switch流程demo
+	 */
 	public static void switchDemo(){
 //		swich语句后面的控制表达式数据类型只能是byte、short、char、int四种整数类型，枚举类型和String类型(从java7开始支持)。不能是boolean类型。
 		int day = 8;
