3092016327d0f399b07899d24ba446e519b5a36d
hchAdmin <Administrator@172.16.133.186>
2017-11-23 18:52:47 +0800

    no message

---
+++ java-foundation/src/main/java/com/orange/foundation/Collections.java
@@ -0,0 +1,16 @@
+package com.orange.foundation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collections {
+	int[] array = new int[]{4,2,23,8888};
+//	List<int> list = new ArrayList<int>(); 编译错误，集合只能存储对象，数组可以存储基本数据类型
+	List<Integer> list = new ArrayList<Integer>();
+	
+	public static void main(String[] args){
+		
+	}
+	
+	
+}
--- java-foundation/src/main/java/com/orange/foundation/ObjectOriented.java
+++ java-foundation/src/main/java/com/orange/foundation/ObjectOriented.java
@@ -1,17 +1,18 @@
 package com.orange.foundation;
 
 public class ObjectOriented {
-	public static void main(String[] args){
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException{
 		Child child = new Child("childA");
 		System.out.println(child.tag); //I'm child!
 		child.myDesc(); //my name is child!
 		System.out.println(child.getName());
 		
+		//属性无多态，而方法具有多态
 		Parent parent = new Child("childB"); 
 		System.out.println(parent.tag); //I'm parent!  此处子类里定义了与父类中已有的变量同名的变量，那么子类中定义的变量会隐藏父类中定义的的变量。不是完全覆盖
 		parent.myDesc(); //my name is child! 此处多态。多态是指方法多态。
-		System.out.println(parent.getName());
-		
+		System.out.println(parent.getName()); //childB  此处getName()也是方法，具有多态
+		System.out.println(parent.getClass()); //class com.orange.foundation.Child  getClass()返回的是运行时类
 		
 	}
 }
@@ -56,6 +57,7 @@ class Child extends Parent{
 		super(name1);
 		name=name1;
 	}
+	public Child(){};
 	@Override
 	public void myDesc(){
 		System.out.println("my name is child!");
