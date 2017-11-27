b154d8a76b1e330560d5189c1544c2dc4b114d7f
hchAdmin <Administrator@172.16.133.186>
2017-11-27 18:00:38 +0800

    no message

--- java-foundation/src/main/java/com/orange/foundation/Collections.java
+++ java-foundation/src/main/java/com/orange/foundation/Collections.java
@@ -9,13 +9,14 @@ import java.util.List;
  *
  */
 public class Collections {
-	int[] array = new int[]{4,2,23,8888};
-//	List<int> list = new ArrayList<int>(); 编译错误，集合只能存储对象，数组可以存储基本数据类型
-	List<Integer> list = new ArrayList<Integer>();
-	
 	public static void main(String[] args){
 		
 	}
 	
+	public static void listDemo(){
+		int[] array = new int[]{4,2,23,8888};
+//		List<int> list = new ArrayList<int>(); 编译错误，集合只能存储对象，数组可以存储基本数据类型
+		List<Integer> list = new ArrayList<Integer>();
+	}
 	
 }
