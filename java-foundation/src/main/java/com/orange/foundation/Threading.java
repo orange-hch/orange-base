c02e55c5e7dd809d5f57479928a134be7292aabb
orange-hch <756236155@qq.com>
2018-04-12 00:26:00 +0800

    no message

--- java-foundation/src/main/java/com/orange/foundation/Threading.java
+++ java-foundation/src/main/java/com/orange/foundation/Threading.java
@@ -1,7 +1,11 @@
 package com.orange.foundation;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 import com.orange.foundation.domain.PrintHelloThread;
 import com.orange.foundation.domain.PrintWorldThread;
+import com.orange.foundation.domain.SumThread;
 
 /**
  * 线程相关
@@ -15,11 +19,24 @@ public class Threading {
 	
 	public static void testThread(){
 		for (int i = 0; i <10 ; i++){
+			//继承Thread类创建线程
 			new PrintHelloThread().start();
 			
+			//实现Runnable接口创建线程
 			PrintWorldThread printWorldThread = new PrintWorldThread();
 			Thread thread = new Thread(printWorldThread, ("printWorldThread" + i));
 			thread.start();
+			
+			SumThread sumThread = new SumThread();
+			FutureTask<Integer> task = new FutureTask<>(sumThread);
+			Thread thread2 = new Thread(task);
+			thread2.start();
+			
+//			try {
+//				System.out.println("Callable子线程返回值：" + task.get());
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
 		}
 	}
 	
---
+++ java-foundation/src/main/java/com/orange/foundation/domain/SumThread.java
@@ -0,0 +1,20 @@
+package com.orange.foundation.domain;
+
+import java.util.concurrent.Callable;
+
+//------------------------实现Callable接口创建线程---------------------
+//通过实现Callable接口创建并启动线程一般步骤如下：
+//1】定义Callable接口的实现类，一样要重写call()方法，这个call（）方法是线程的执行体,且该call()方法有返回值。
+//2】创建Callable实现类的实例，使用FutureTask类来包装Callable对象，该FutureTask对象封装了该Callable对象的call()方法的返回值。
+//3】使用FutureTask对象作为Thread对象的target创建线程，并调用start()启动新线程。
+//4】调用FutureTask对象的get()方法来获得子线程执行结束后的返回值。
+public class SumThread implements Callable<Integer> {
+	@Override
+	public Integer call() {
+		Integer sum = 0;
+		for(int i = 1; i <= 100; i++){
+			sum += i;
+		}
+		return sum;
+	}
+}
